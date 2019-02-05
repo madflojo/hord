a39591aaf6fdc8057179de9b1dcbd206604f1459
Benjamin Cane <ben@bencane.com>
2019-02-04 22:20:21 -0700

    Adding Set functionality

--- app/grpc.go
+++ app/grpc.go
@@ -3,10 +3,12 @@ package app
 import (
 	"context"
 	"fmt"
+	"github.com/madflojo/hord/databases"
 	pb "github.com/madflojo/hord/proto/client"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"net"
+	"time"
 )
 
 // server is used to implement the client protobuf server interface
@@ -63,7 +65,39 @@ func (s *grpcServer) Get(ctx context.Context, msg *pb.GetRequest) (*pb.GetRespon
 
 // Set will take the supplied data and store it within the datastore returning success or failure
 func (s *grpcServer) Set(ctx context.Context, msg *pb.SetRequest) (*pb.SetResponse, error) {
-	return &pb.SetResponse{}, nil
+	// Define reply message
+	r := &pb.SetResponse{
+		Status: &pb.Status{
+			Code:        0,
+			Description: "Success",
+		},
+	}
+
+	// Check key length
+	if len(msg.Key) == 0 {
+		log.WithFields(logrus.Fields{"key": msg.Key}).Tracef("Key %s is not defined", msg.Key)
+		r.Status.Code = 4
+		r.Status.Description = "Key must be defined"
+		return r, fmt.Errorf("Key %s is not defined", msg.Key)
+	}
+
+	// Create data item for insertion
+	d := &databases.Data{}
+	d.Data = msg.Data
+	d.LastUpdated = time.Now().UnixNano()
+
+	// Insert data into datastore
+	// TODO: Handle TTL settings
+	err := db.Set(msg.Key, d)
+	if err != nil {
+		log.WithFields(logrus.Fields{"key": msg.Key, "error": err}).Tracef("Failed to store data for key - %s", err)
+		r.Status.Code = 5
+		r.Status.Description = "Error storing data within datastore"
+		return r, fmt.Errorf("Error storing data within datastore - %s", err)
+	}
+
+	r.Key = msg.Key
+	return r, nil
 }
 
 // Delete will remove the specified key from the datastore and return success or failure
